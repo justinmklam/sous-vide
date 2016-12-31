01e31375ad292d48175ef4de2b7664a009143e10
justinmklam <justinmklam@gmail.com>
2016-12-31 11:56:06 -0800

    Temperature data now publishes to adafruit.io through MQTT.

--- SousVideESP8266/SousVideESP8266.ino
+++ SousVideESP8266/SousVideESP8266.ino
@@ -15,6 +15,20 @@ Author: Justin Lam
 #include <DallasTemperature.h>
 #include <Encoder.h>
 #include "CountdownTimer.h"
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+// Wifi access point
+#define WLAN_SSID       "HouseOfLams-2.4G"
+#define WLAN_PASS       "1amfami1y"
+
+// Adafruit.io setup
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883                   // use 8883 for SSL
+#define AIO_USERNAME    "justinmklam"
+#define AIO_KEY         "e6023bd3c2354a79adeb6b8ea95223cd"
+
 
 #define LCD_COLS 16     // LCD dimensions
 #define LCD_ROWS 2      // LCD dimensions
@@ -30,6 +44,7 @@ Author: Justin Lam
 #define ENCODER_STEPS_TIME 0.16
 #define DEG_SYMBOL (char)223    // character code for degree symbol for LCD
 #define REFRESH_RATE_UI 100     // refresh the UI every 100 ms
+#define MQTT_PUBLISH_FREQ 15000  // publish data to MQTT every 15 secs
 #define STARTING_TEMP 65
 #define MAX_TEMP 100
 #define MIN_TEMP 20
@@ -38,14 +53,18 @@ Author: Justin Lam
 
 OneWire oneWire(TEMP_SENSOR_PIN);
 DallasTemperature DS18B20(&oneWire);
-
 LiquidCrystal_I2C lcd(I2C_ADDR, LCD_COLS, LCD_ROWS);
-
 Encoder myEnc(ENCODER_PIN_A, ENCODER_PIN_B);
-
 CountDownTimer timer;
 
-bool pauseMode = false;
+// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+WiFiClient client;
+// Setup the MQTT client
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+Adafruit_MQTT_Publish tempMQTT = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/sous-vide");
+
+bool pauseMode = true;
 bool timerEnd = false;
 bool buttonPressed = false;
 bool modeChange = false;
@@ -76,7 +95,29 @@ uint32_t set_min = 0;
 // Buffer for printing to LCD
 char buf[40];
 
+// Bug workaround for Arduino 1.6.6, it seems to need a function declaration
+// for some reason (only affects ESP8266, likely an arduino-builder bug).
+void MQTT_connect();
+
 void setup() {
+    Serial.begin(115200);
+    delay(10);
+
+    // Connect to WiFi access point.
+    Serial.println(); Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(WLAN_SSID);
+
+    WiFi.begin(WLAN_SSID, WLAN_PASS);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println();
+
+    Serial.println("WiFi connected");
+    Serial.println("IP address: "); Serial.println(WiFi.localIP());
+
     pinMode(BUTTON_PIN, INPUT_PULLUP);
     pinMode(RELAY_PIN, OUTPUT);
     digitalWrite(RELAY_PIN,HIGH);   // relay is switched off on HIGH
@@ -93,7 +134,7 @@ void setup() {
 
     attachInterrupt(BUTTON_PIN, buttonISR, FALLING);
 
-    timer.SetTimer(0,0,3);
+    timer.SetTimer(0,0,0);
     timer.StartTimer();
 }
 
@@ -165,9 +206,9 @@ void setNewTime() {
         pauseMode = false;
     }
 
-    lcd.print("Set new time");
+    lcd.print("Set new time:");
     lcd.setCursor(0,1);
-    sprintf(buf,"New time: %dh%d       ",set_hr,set_min);
+    sprintf(buf,"  t = %dh%d               ",set_hr,set_min);
     lcd.print(buf);
 }
 
@@ -184,24 +225,25 @@ void setNewTemp() {
     set_temp = readEncoderTemp() + temp_offset;
     old_temp = set_temp;
 
-    lcd.print("Set new temp");
+    lcd.print("Set new temp:");
     lcd.setCursor(0,1);
-    sprintf(buf,"New temp: %d%cC     ",set_temp, DEG_SYMBOL);
+    sprintf(buf,"  T = %d%cC               ",set_temp, DEG_SYMBOL);
     lcd.print(buf);
 }
 
 // Show live cooking temperature and time status
 void monitorStatus() {
     temp_reading = readTempSensor();
-
     controlTemp(temp_reading, set_temp);
+
+    MQTT_publish(temp_reading);
 
     // Construct char buffer to convert float to string
     char t[10];
     dtostrf(temp_reading, 4, 1, t);
 
     // Show temperature status
-    sprintf(buf,"T| %s%cC (%d%cC)     ",t, DEG_SYMBOL, set_temp, DEG_SYMBOL); 
+    sprintf(buf,"T  %s%cC (%d%cC)     ",t, DEG_SYMBOL, set_temp, DEG_SYMBOL); 
     lcd.setCursor(0,0); 
     lcd.print(buf);
 
@@ -220,11 +262,12 @@ void monitorStatus() {
     }
 
     // Show time status
-    sprintf(buf,"t| %d:%d:%d       ",timer.ShowHours(),timer.ShowMinutes(),timer.ShowSeconds());
+    sprintf(buf,"t  %d:%d:%d       ",timer.ShowHours(),timer.ShowMinutes(),timer.ShowSeconds());
     lcd.setCursor(0,1);
     lcd.print(buf);
 }
 
+// PID temperature control
 void controlTemp(float temp_reading, float desired_temp) {
     if (temp_reading < desired_temp) {
         digitalWrite(RELAY_PIN,LOW);
@@ -234,8 +277,9 @@ void controlTemp(float temp_reading, float desired_temp) {
     }
 }
 
+// Flash the LCD backlight when timer is done
 void blinkLCD(uint8_t blink_rate) {
-    static uint32_t current_millis = millis();
+    uint32_t current_millis = millis();
     static uint32_t previous_millis = 0;
 
     if ((current_millis - previous_millis) >= blink_rate) {
@@ -247,7 +291,48 @@ void blinkLCD(uint8_t blink_rate) {
             lcd.backlight();
             backlightOn = true;
         }
+        previous_millis = current_millis;
     }
+}
+
+// Publish data through MQTT to Adafruit.io
+void MQTT_publish(float val) {
+    uint32_t current_millis = millis();
+    static uint32_t previous_millis = 0;
+
+    if ((current_millis - previous_millis) >= MQTT_PUBLISH_FREQ) {
+        MQTT_connect(); // reconnect if necessary
+
+        if(!tempMQTT.publish(val)) {
+            Serial.println(F("Failed"));
+        }
+        previous_millis = current_millis;
+    }
+}
+
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+       retries--;
+       if (retries == 0) {
+         // basically die and wait for WDT to reset me
+         while (1);
+       }
+  }
+  Serial.println("MQTT Connected!");
 }
 
 void loop() {
@@ -256,7 +341,7 @@ void loop() {
     // timer.StopTimerAt(0,0,0);
 
     if(!pauseMode && !timerEnd && modeUI == 0) {
-        blinkLCD(500);
+        blinkLCD(100);
     }
     else if(!backlightOn) {
         lcd.backlight();
